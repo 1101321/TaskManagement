f7f6739b598ac12806020914e4c2404d6a03aec7
1101321 <hcri0909@gmail.com>
2022-02-16 19:06:21 +0000

    [Add] ログイン・ログアウト機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root to: 'tasks#index'
   resources :tasks
+  resources :users, only: [:show, :edit, :update]
+  get '/sign_up' => 'users#new'
+  post '/sign_up' => 'users#create'
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
